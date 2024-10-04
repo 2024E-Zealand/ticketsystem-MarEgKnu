022d579850100f6ff68c9c61f418b598e879bf08
Marius Egede Knudsen <mak004@edu.zealand.dk>
2024-10-04 14:50:37 +0200

    MCtests

---
+++ TicketClassLibraryTests/MCTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketClassLibrary;
+
+namespace TicketClassLibraryTests
+{
+    [TestClass]
+    public class MCTests
+    {
+        [TestMethod()]
+        public void PriceTest_Returns125()
+        {
+            MC mc = new MC();
+            double expected = 125;
+            double actual = mc.Price();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void VehicleTypeTest_ReturnsMC()
+        {
+            MC mc = new MC();
+            string expected = "MC";
+            string actual = mc.VehicleType();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
